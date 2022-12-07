5a0947ebf02301b4f5e0a165b52222c913249342
shariqredstaglabs <101240973+shariqredstaglabs@users.noreply.github.com>
2022-12-07 20:52:18 +0530

    project structure

---
+++ resellme (1)/resellme/src/main/java/com/resellme/resellme/ResellmeApplication.java
@@ -0,0 +1,13 @@
+package com.resellme.resellme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellmeApplication.class, args);
+	}
+
+}
---
+++ resellme (1)/resellme/src/test/java/com/resellme/resellme/ResellmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resellme.resellme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
